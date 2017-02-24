2e5a18f7f58590e4537f12662c3eae22267162bc
Tim Sandgren <tim.sndgrn@gmail.com>
2017-02-24 19:59:51 +0100

    misc

--- gen.py
+++ gen.py
@@ -8,35 +8,35 @@ from lib import jumblers
 
 # ------------------------------------------------------------------------------
 
-def generate(order_dct):
-    all_words = data_mgm.path_to_wordlist(order_dct["root_wordlist_path"])
-    wordlist_a = data_mgm.path_to_wordlist(order_dct["wordlist_a_path"])
-    wordlist_b = data_mgm.path_to_wordlist(order_dct["wordlist_b_path"])
+def generate(order):
+    all_words = data_mgm.path_to_wordlist(order["root_wordlist_path"])
+    wordlist_a = data_mgm.path_to_wordlist(order["wordlist_a_path"])
+    wordlist_b = data_mgm.path_to_wordlist(order["wordlist_b_path"])
 
     mixed = jumblers.get_multiword(wordlist_a[0],
                                    wordlist_b[0],
-                                   int(order_dct["word_count_mixed"]))
+                                   int(order["word_count_mixed"]))
 
     morph = jumblers.get_morphword(wordlist_a[0],
                                    wordlist_b[0],
-                                   int(order_dct["word_count_morph"]))
+                                   int(order["word_count_morph"]))
 
-    clean_mixed = data_mgm.wash_wlist(mixed, order_dct["remove"])
-    clean_morph = data_mgm.wash_wlist(morph, order_dct["remove"])
+    clean_mixed = data_mgm.wash_wlist(mixed, order["remove"])
+    clean_morph = data_mgm.wash_wlist(morph, order["remove"])
 
-    if order_dct["save_to_disk"]:
-        data_mgm.write_text(order_dct["output_path_mixed"], str(clean_mixed))
-        data_mgm.write_text(order_dct["output_path_morph"], str(clean_morph))
+    if order["save_to_disk"]:
+        data_mgm.write_text(order["output_path_mixed"], str(clean_mixed))
+        data_mgm.write_text(order["output_path_morph"], str(clean_morph))
 
     return clean_mixed, clean_morph
 
 
 def main(args):
-    order_dct = data_mgm.json_to_dict(args[1])
-    wrds = generate(order_dct)
+    order = data_mgm.json_to_dict(args[1])
+    wrds = generate(order)
 
-    if int(order_dct["mode"]) == 0:
-        if order_dct["print_to_console"]:
+    if int(order["mode"]) == 0:
+        if order["print_to_console"]:
             print("")
             print("Mixed: " + str(wrds[0]))
             print("")
